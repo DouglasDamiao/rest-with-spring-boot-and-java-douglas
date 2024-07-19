ea24176c6371803d9df59e07ffd8177db569c5a9
DouglasDamiao <douglas_estudodev@hotmail.com>
2024-07-18 22:25:34 -0300

    # Please enter the commit message for your changes. Lines starting

--- 05_SpringBoot/src/main/java/br/com/douglas/Greeting.java
+++ 05_SpringBoot/src/main/java/br/com/douglas/Greeting.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package br.com.douglas;
 
 public class Greeting {
@@ -26,3 +27,33 @@ public class Greeting {
 	
 	
 }
+=======
+package br.com.douglas;
+
+public class Greeting {
+
+	private final long id;
+	private final String content ;
+	
+	public Greeting(long id, String content) {	
+		this.id = id;
+		this.content = content;
+	}
+
+	/**
+	 * @return o id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @return o content
+	 */
+	public String getContent() {
+		return content;
+	} 
+	
+	
+}
+>>>>>>> f10b4ff05b964fdf49f024794c794d02d06c56c6
--- 05_SpringBoot/src/main/java/br/com/douglas/GreetingController.java
+++ 05_SpringBoot/src/main/java/br/com/douglas/GreetingController.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package br.com.douglas;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -22,3 +23,29 @@ public class GreetingController{
 	
 }
 
+=======
+package br.com.douglas;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class GreetingController{
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "World")
+			String name) {
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
+	}
+	
+}
+
+>>>>>>> f10b4ff05b964fdf49f024794c794d02d06c56c6
